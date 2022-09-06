c58b94ab0d25a78e12db4bb6161ae0bdf9f1ad6a
Jonathan Cardoso <jonathan99mour@gmail.com>
2022-09-06 08:30:38 -0300

    feat: (Resolvendo problema absolute props e template para as paginas)

---
+++ src/pages/[slug].tsx
@@ -0,0 +1,44 @@
+import client from 'graphql/client'
+import PageTemplate, { PageTemplateProps } from 'templates/Pages'
+import { GET_PAGE_BY_SLUG, GET_PAGES } from 'graphql/queries'
+import { useRouter } from 'next/router'
+import { GetStaticProps } from 'next'
+
+export default function Page({ heading, body }: PageTemplateProps) {
+  const router = useRouter()
+
+  // retorna loading, enquanto esta criando
+  if (router.isFallback) return null
+
+  return <PageTemplate heading={heading} body={body} />
+}
+export async function getStaticPaths() {
+  const { pages } = await client.request(GET_PAGES, { first: 3 })
+
+  const paths = pages.map(({ slug }) => ({
+    params: { slug }
+  }))
+
+  // gerando as urls
+  return { paths, fallback: true }
+}
+
+export const getStaticProps: GetStaticProps = async ({ params }) => {
+  const { page } = await client.request(GET_PAGE_BY_SLUG, {
+    slug: `${params?.slug}`
+  })
+
+  if (!page) return { notFound: true }
+
+  return {
+    props: {
+      heading: page.heading,
+      body: page.body.html
+    }
+  }
+}
+
+// getStaticPaths - gerar urls build time
+// getStaticProps - busca de dados da pagina (props) build time
+// getServerSideProps - busca de dados da pagina (props) run time, bundle também no serve
+// getInitialProps - mesmo que getStaticProps e getServerSideProps, bundle também no client
--- src/pages/index.tsx
+++ src/pages/index.tsx
@@ -1,30 +1,37 @@
 import dynamic from 'next/dynamic'
+import LinkWrapper from '../components/LinkWrapper'
+import { InfoOutline } from '@styled-icons/evaicons-outline'
 
 const Map = dynamic(() => import('components/Map'), { ssr: false })
 
 export default function Home() {
   return (
-    <Map
-      places={[
-        {
-          id: '1',
-          name: 'Natal',
-          slug: 'natal',
-          location: {
-            latitude: 0,
-            longitude: 0
+    <>
+      <LinkWrapper href="/about">
+        <InfoOutline size={32} aria-label="About" />
+      </LinkWrapper>
+      <Map
+        places={[
+          {
+            id: '1',
+            name: 'Natal',
+            slug: 'natal',
+            location: {
+              latitude: 0,
+              longitude: 0
+            }
+          },
+          {
+            id: '2',
+            name: 'Mossoró',
+            slug: 'mossoro',
+            location: {
+              latitude: 2,
+              longitude: 1
+            }
           }
-        },
-        {
-          id: '2',
-          name: 'Mossoró',
-          slug: 'mossoro',
-          location: {
-            latitude: 2,
-            longitude: 1
-          }
-        }
-      ]}
-    />
+        ]}
+      />
+    </>
   )
 }
